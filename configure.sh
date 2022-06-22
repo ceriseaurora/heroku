1d34fd2034e1b8d04593ed5b2b5f248b52259b44
Clown1i <102837000+Clown1i@users.noreply.github.com>
2022-06-23 03:40:38 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,6 +8,9 @@ install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
 # Remove temporary directory
 rm -rf /tmp/xray
+# xray new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
 {
     "log": {
         "loglevel": "none"
@@ -78,59 +81,6 @@ rm -rf /tmp/xray
         }
     ]
 }
-}
-EOF
-# xray new configuration
-install -d /usr/local/etc/xray
-cat << EOF > /usr/local/etc/xray/config.json
-{
-    "log": {
-        "loglevel": "none"
-    },
-    "inbounds": [
-        {
-            "port": ${PORT},
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                      "id": "${id}",
-                      "alterId": 0,
-                      "email": "love@xray.com"
-                    }
-                  ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "${path}"
-                }
-          }
-        },
-        {
-            "port": ${PORT},
-            "protocol": "trojan",
-            "settings": {
-                "clients": [
-                    {
-                     "password": "${password}",
-                      "flow": "xtls-rprx-direct"
-                    }
-                  ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "${path}"
-                }
-          }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "Freedom"
-        }
-    ]
 }
 EOF
 
